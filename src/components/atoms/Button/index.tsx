1f1e1d02b9467c7ff4da2eda419240fad813a26c
Éverton Toffanetto <evertondgn@hotmail.com>
2024-04-17 20:57:04 -0300

    feat: upgrade eleganceui-tailwind-utilities and adjust font-weight styles
    Upgraded the 'eleganceui-tailwind-utilities' dependency from version 3.1.1 to 5.0.0. As a result, adjustments were made to font-weight class usage in different components. The 'fs-3xl-medium' style has been replaced with 'fs-3xl fw-medium' and 'fs-md-medium-white' with 'fs-md tx-white fw-medium'.

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -4,7 +4,7 @@ import type { ButtonProps } from './types'
 
 export const Button = ({ label, ...props }: ButtonProps) => (
   <button
-    className="fs-md-medium-white flex size-fit cursor-pointer bg-sky-700 px-10 py-4 transition-colors ease-in-out br-md center hover:bg-sky-500 active:bg-sky-700"
+    className="fs-md tx-white flex size-fit cursor-pointer bg-sky-700 px-10 py-4 transition-colors ease-in-out br-md center fw-medium hover:bg-sky-500 active:bg-sky-700"
     {...props}
   >
     {label}
--- src/components/organisms/Error/index.tsx
+++ src/components/organisms/Error/index.tsx
@@ -6,11 +6,11 @@ import type { ErrorProps } from './types'
 
 export const Error = ({ reset }: ErrorProps) => (
   <div className="min-h-screen p-8 col-full center">
-    <header className="mb-5 fs-3xl-medium" role="banner">
+    <header className="mb-5 fs-3xl fw-medium" role="banner">
       Error!
     </header>
     <main className="p-0 ai-center col-full g-5">
-      <h1 className="fs-3xl-medium tx-center">Page not found!</h1>
+      <h1 className="fs-3xl fw-medium tx-center">Page not found!</h1>
       <Button label="Try again" onClick={reset} />
     </main>
   </div>
